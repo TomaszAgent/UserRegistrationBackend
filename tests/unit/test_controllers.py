fb9112e29971acca12fb2b734ec0fd98028637af
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-02-18 18:55:49 +0100

    add tests for type errors

--- tests/unit/test_controllers.py
+++ tests/unit/test_controllers.py
@@ -17,7 +17,7 @@ def create_user_controller(users_repository: Mock) -> CreateUserController:
     return CreateUserController(repository=users_repository)
 
 
-def test_create_user_controller_exists():
+def test_create_user_controller_exists() -> None:
     assert create_user_controller
 
 
@@ -27,3 +27,27 @@ def test_create_user_controller_passes_user(
 ) -> None:
     create_user_controller.create("test", "test", 2000, "user")
     users_repository.add_user.assert_called_with("test", "test", 2000, "user")
+
+
+def test_create_user_raises_first_name_type_error(create_user_controller: CreateUserController) -> None:
+    with pytest.raises(TypeError) as actual:
+        create_user_controller.create(0, "test", 2000, "user")
+    assert str(actual.value) == "One of arguments was of invalid type."
+
+
+def test_create_user_raises_last_name_type_error(create_user_controller: CreateUserController) -> None:
+    with pytest.raises(TypeError) as actual:
+        create_user_controller.create("test", 0, 2000, "user")
+    assert str(actual.value) == "One of arguments was of invalid type."
+
+
+def test_create_user_raises_birth_year_type_error(create_user_controller: CreateUserController) -> None:
+    with pytest.raises(TypeError) as actual:
+        create_user_controller.create("test", "test", "test", "user")
+    assert str(actual.value) == "One of arguments was of invalid type."
+
+
+def test_create_user_raises_group_type_error(create_user_controller: CreateUserController) -> None:
+    with pytest.raises(TypeError) as actual:
+        create_user_controller.create("test", "test", 2000, 0)
+    assert str(actual.value) == "One of arguments was of invalid type."
