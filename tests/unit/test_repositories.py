43bbd643fe63f1c6aad95c31b523782ec17cc485
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-02-18 15:53:32 +0100

    create tests for users repository

--- tests/unit/test_repositories.py
+++ tests/unit/test_repositories.py
@@ -1,6 +1,25 @@
+import pytest
+
 from src.repositories import UsersRepository
 
 
-def test_user_repository_exists() -> None:
+@pytest.fixture()
+def users_repository() -> UsersRepository:
+    return UsersRepository()
+
+
+def test_users_repository_exists() -> None:
     assert UsersRepository
-    
+
+
+def test_users_repository_adds_users(users_repository: UsersRepository) -> None:
+    users_repository.add_user('test', 'test', 2000, 'user')
+    assert users_repository.get_users() == [
+        {
+            'id': 0,
+            'first_name': 'test',
+            'last_name': 'test',
+            'birth_year': 2000,
+            'group': 'user'
+        }
+    ]
