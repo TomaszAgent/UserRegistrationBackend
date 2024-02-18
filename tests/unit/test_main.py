0cedf08c34f27353a61a10e5ab33912ca533650e
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-02-18 15:31:15 +0100

    add tests for ping

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,5 +1,11 @@
 from src.main import ping
 
-def test_ping() -> None:
+
+def test_ping_exists() -> None:
+    actual = ping
+    assert actual
+
+
+def test_ping_returns_right_value() -> None:
     actual = ping()
     assert actual == "pong"
