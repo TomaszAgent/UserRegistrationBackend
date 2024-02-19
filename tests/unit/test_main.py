4868cf401bf8488b777b0608eb43fe2d8cb52dcd
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-02-19 22:19:53 +0100

    fix get users tests

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,7 +1,10 @@
-from unittest.mock import patch
+from unittest.mock import patch, MagicMock, _patch, AsyncMock
+
+import pytest
 
 import json
 
+import src.main
 from src.main import ping, app, get_users
 from src.STATUS_CODES import OK
 
@@ -16,15 +19,15 @@ def test_ping_returns_right_value() -> None:
 
 
 def test_get_users_returns_right_code() -> None:
-    with patch("src.main.get_users_controller") as class_mock:
-        class_mock.get.return_value = []
+    with patch("src.main.get_users_controller") as get_users_controller:
+        get_users_controller.get.return_value = []
         actual = get_users().status
     assert actual == OK
 
 
-def test_get_users_return_right_value() -> None:
-    with patch("src.main.get_users_controller") as class_mock:
-        class_mock.get.return_value = [
+def test_get_users_returns_right_value() -> None:
+    with patch("src.main.get_users_controller") as get_users_controller:
+        get_users_controller.get.return_value = [
             {
                 "id": 0,
                 "first_name": "test",
