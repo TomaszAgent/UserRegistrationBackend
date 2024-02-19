338c83bf177cd3eae875958f8351af6971fa5065
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-02-19 21:58:18 +0100

    implement get users function

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from flask import Flask
+from flask import Flask, Response
+import json
 
-from src.controllers import CreateUserController, GetUsersController, UpdateUserController, DeleteUserController
+from src.controllers import create_users_controller, get_users_controller, update_users_controller, delete_users_controller
+from src.STATUS_CODES import OK
 
 app = Flask(__name__)
 
@@ -8,6 +10,12 @@ app = Flask(__name__)
 @app.get('/ping')
 def ping() -> str:
     return 'pong'
+
+
+@app.get('/users')
+def get_users() -> Response:
+    users = get_users_controller.get()
+    return Response(response=json.dumps(users), status=OK, mimetype='application/json')
 
 
 if __name__ == "__main__":
