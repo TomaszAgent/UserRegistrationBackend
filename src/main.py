27a71a9a3ec59277d3e62fad7e6c981877cf7da7
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-02-19 22:32:10 +0100

    implement get user

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from flask import Flask, Response
 import json
 
 from src.controllers import create_users_controller, get_users_controller, update_users_controller, delete_users_controller
-from src.STATUS_CODES import OK
+from src.STATUS_CODES import OK, BAD_REQUEST
 
 app = Flask(__name__)
 
@@ -16,6 +16,15 @@ def ping() -> str:
 def get_users() -> Response:
     users = get_users_controller.get()
     return Response(response=json.dumps(users), status=OK, mimetype='application/json')
+
+
+@app.get('/users/<id>')
+def get_user(id: int) -> Response:
+    try:
+        user = get_users_controller.get(id=int(id))
+        return Response(response=json.dumps(user), status=OK, mimetype='application/json')
+    except ValueError:
+        return Response(status=BAD_REQUEST)
 
 
 if __name__ == "__main__":
