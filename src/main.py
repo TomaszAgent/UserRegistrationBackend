01ad4301418a4936cecf041568591fc7a855cb2d
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-02-20 14:07:46 +0100

    implement post user

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import json
 
 from src.controllers import create_users_controller, get_users_controller, update_users_controller, delete_users_controller
-from src.STATUS_CODES import OK, BAD_REQUEST
+from src.STATUS_CODES import OK, BAD_REQUEST, CREATED
 
 app = Flask(__name__)
 
@@ -23,8 +23,18 @@ def get_user(id: int) -> Response:
     try:
         user = get_users_controller.get(id=int(id))
         return Response(response=json.dumps(user), status=OK, mimetype='application/json')
-    except ValueError:
-        return Response(status=BAD_REQUEST)
+    except ValueError as error:
+        return Response(response=str(error), status=BAD_REQUEST)
+
+
+@app.post("/users")
+def post_user() -> Response:
+    data = request.get_json()
+    try:
+        create_users_controller.create(data)
+        return Response(status=CREATED)
+    except (ValueError, TypeError) as error:
+        return Response(response=str(error), status=BAD_REQUEST)
 
 
 if __name__ == "__main__":
