e728dac1fed80ea546d97ddc74a0dd5ea6864f32
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-02-20 18:06:32 +0100

    implement delete

--- src/main.py
+++ src/main.py
@@ -18,10 +18,10 @@ def get_users() -> Response:
     return Response(response=json.dumps(users), status=OK, mimetype='application/json')
 
 
-@app.get('/users/<id>')
+@app.get('/users/<int:id>')
 def get_user(id: int) -> Response:
     try:
-        user = get_users_controller.get(id=int(id))
+        user = get_users_controller.get(id=id)
         return Response(response=json.dumps(user), status=OK, mimetype='application/json')
     except ValueError as error:
         return Response(response=str(error), status=BAD_REQUEST)
@@ -37,13 +37,22 @@ def post_user() -> Response:
         return Response(response=str(error), status=BAD_REQUEST)
 
 
-@app.patch("/users/<id>")
+@app.patch("/users/<int:id>")
 def patch_user(id: int) -> Response:
     data = request.get_json()
     try:
         update_users_controller.update(id, data)
         return Response(status=NO_CONTENT)
     except (ValueError, TypeError) as error:
+        return Response(response=str(error), status=BAD_REQUEST)
+
+
+@app.delete("/users/<int:id>")
+def delete_user(id: int) -> Response:
+    try:
+        delete_users_controller.delete(id)
+        return Response(status=NO_CONTENT)
+    except ValueError as error:
         return Response(response=str(error), status=BAD_REQUEST)
 
 
